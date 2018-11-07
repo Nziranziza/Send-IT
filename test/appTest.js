8f6a10713fa2d157990888922119c7f05219ff98
Nziranziza <nziranzizadaniel@gmail.com>
2018-11-07 13:14:37 +0200

    Testing get request

--- test/appTest.js
+++ test/appTest.js
@@ -8,9 +8,63 @@ chai.use(chaiHttp);
 
 /* Test the /GET route */
 describe('app index route', () => {
-  it('it should GET /', (done) => {
+  it('it should GET Home page', (done) => {
     chai.request(app)
       .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('it should GET Login Page', (done) => {
+    chai.request(app)
+      .get('/login')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('it should GET Contact us Page', (done) => {
+    chai.request(app)
+      .get('/contact-us')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('it should GET Sing up Page ', (done) => {
+    chai.request(app)
+      .get('/sign-up')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('it should GET About us Page ', (done) => {
+    chai.request(app)
+      .get('/sign-up')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('it should GET Admin Dashboard Page ', (done) => {
+    chai.request(app)
+      .get('/sign-up')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('it should GET User Dashboard Page ', (done) => {
+    chai.request(app)
+      .get('/sign-up')
       .end((err, res) => {
         res.should.have.status(200);
         done();
