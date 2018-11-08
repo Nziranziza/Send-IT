5fb79eda9ee0a8401b6e18da519aee275f77303c
Nziranziza <nziranzizadaniel@gmail.com>
2018-11-08 11:06:05 +0200

    export routes

--- routes/api.js
+++ routes/api.js
@@ -4,16 +4,18 @@ import parcelController from '../controllers/ParcelController';
 const router = express.Router();
 
 /* Fetch all parcel delivery orders */
-router.get('/parcels', parcelController.getAll);
+router.get('/v1/parcels', parcelController.getAll);
 
 /* Fetch a specific parcel delivery order */
-router.get('/parcels/:id', parcelController.getOne);
+router.get('/v1/parcels/:id', parcelController.getOne);
 
 /* Cancel the specific parcel delivery order */
-router.put('/parcels/:id/cancel', parcelController.update);
+router.put('/v1/parcels/:id/cancel', parcelController.update);
 
 /* Fetch all parcel delivery orders by a specific user */
-router.get('/users/:userId/parcels', parcelController.getAll);
+router.get('/v1/users/:userId/parcels', parcelController.getAll);
 
 /* GET All Parcels */
-router.post('/parcels', parcelController.create);
+router.post('/v1/parcels', parcelController.create);
+
+export default router;
