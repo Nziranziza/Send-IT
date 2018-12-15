c96132e634ba153864634ebdf248728c586fb8e2
Nziranziza <nziranzizadaniel@gmail.com>
2018-12-15 17:47:46 +0200

    [feature #162609618] add frontEnd login page

---
+++ UI/js/login.js
@@ -0,0 +1,30 @@
+async function getData() {
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+  const header = new Headers();
+
+  header.append('Accept', 'application/json');
+  header.append('Content-Type', 'application/json');
+  const response = await fetch('../../api/v1/auth/login', {
+    method: 'post',
+    headers: header,
+    body: JSON.stringify({
+      email,
+      password
+    })
+  });
+  const data = await response.json();
+  return data;
+}
+function login() {
+  getData()
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem('sendit-user-token', JSON.stringify(data));
+        window.location.replace('/pages/createparcel.html');
+      } else {
+        const errarea = document.getElementById('err-msg');
+        errarea.innerHTML = data.message;
+      }
+    });
+}
